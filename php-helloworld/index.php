a90f331bb502f3a4eeaf132475beda87da05d4c6
Lautemann1 <85985893+Lautemann1@users.noreply.github.com>
2021-12-12 14:12:27 +0100

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, World! Really NEW UPDADATE!!! php version is " . PHP_VERSION . "\n";
+print "Hello, World! Really NEW UPDADATE just in git to test triggers!!! php version is " . PHP_VERSION . "\n";
 ?>
